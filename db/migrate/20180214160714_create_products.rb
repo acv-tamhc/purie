7af06b0f3b554344ebd7380190797a9abfbdae6f
acv-tamhc <acv-tamhc@atmarkcafe.org>
2018-02-14 23:29:18 +0700

    init db product

---
+++ db/migrate/20180214160714_create_products.rb
@@ -0,0 +1,28 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+    	t.integer :item_id
+    	t.string 	:group_nav
+    	t.integer :group_id
+    	t.string	:item_related
+    	t.string	:item_code
+    	t.string	:picture
+    	t.string	:galleries
+    	t.string	:title
+    	t.float		:price
+    	t.float		:price_buy
+    	t.float		:price_sale
+    	t.text		:short
+    	t.text		:content
+    	t.string	:link
+    	t.string	:meta_title
+    	t.string	:meta_key
+    	t.string	:meta_desc
+    	t.integer	:in_stock
+    	t.integer	:out_stock
+    	t.integer	:viewer
+    	t.string	:lang
+    	t.timestamps
+    end
+  end
+end

b63dd1e56b8f62d0745bcc12a8027496c96b439e
acv-tamhc <acv-tamhc@atmarkcafe.org>
2018-03-22 11:55:04 +0700

    get product data Amazon

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,24 @@ Bundler.require(*Rails.groups)
 require 'awesome_print'
 require 'amazon/ecs'
 require 'nokogiri'
+require 'xmlsimple'
+
+# get product
+require 'time'
+require 'uri'
+require 'openssl'
+require 'base64'
+
+# Your Access Key ID, as taken from the Your Account page
+ACCESS_KEY_ID = "AKIAJWPWTVNYFLW7EKHQ"
+
+# Your Secret Key corresponding to the above ID, as taken from the Your Account page
+SECRET_KEY = "C9ZIPiqI5j31xbH8N83rNOzp4XAQ8FuRlVUGlMTy"
+
+# The region you are interested in
+ENDPOINT = "webservices.amazon.com"
+
+REQUEST_URI = "/onca/xml"
 
 Amazon::Ecs.configure do |options|
   options[:AWS_access_key_id] = 'AKIAJWPWTVNYFLW7EKHQ'

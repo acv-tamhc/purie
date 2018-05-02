6750f4f0c4cddc27543c9d1afcd86688a5a8a606
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-02 14:13:31 +0700

    order detail rspec

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
-	belongs_to :order, dependent: :destroy
-	belongs_to :product, dependent: :destroy
+	belongs_to :order, dependent: :destroy, optional: true
+	belongs_to :product, dependent: :destroy, optional: true
 	validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 
 	def get_total

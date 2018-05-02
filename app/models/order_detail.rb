35a74ae8b740100c13fb66ef3a1afefef5c1c2b6
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-02 17:21:48 +0700

    add remove cart

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
 	belongs_to :order, dependent: :destroy, optional: true
-	belongs_to :product, dependent: :destroy, optional: true
+	belongs_to :product, dependent: :destroy
 	validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 
 	def get_total

c93fc398e56105b5d35d6b1ad5d61a7a7ff3f6c0
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-02 14:15:02 +0700

    change product

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Category, type: :model do
 	let!(:category) { create(:category) }
 	it { should validate_presence_of(:title) }
 	it { should validate_presence_of(:description) }
-	it { should have_many(:product)}
+	it { should have_many(:product) }
 
-	it 'Can\'t be description than title ' do
+	it 'Can\'t be description than title' do
 		category.save!
 		expect(category.title_is_description_than_short).to eq nil
 	end

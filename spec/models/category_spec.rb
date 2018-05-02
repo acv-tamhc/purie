493f59fa100af570c43c9c496577ebe67d0d72bc
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-02 10:38:55 +0700

    title is description than short

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Category, type: :model do
 	let!(:category) { create(:category) }
 	it { should validate_presence_of(:title) }
 	it { should validate_presence_of(:description) }
+
+	it 'Can\'t be description than title ' do
+		category.save!
+		expect(category.title_is_description_than_short).to eq nil
+	end
 end

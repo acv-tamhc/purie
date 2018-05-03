fa113308e11e3d3ab8dab173bb0d9a10dea7a153
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-03 23:11:51 +0700

    fix index - order - factories .

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
-	factory :order do
-		
-		email "test@mail.com"
-		phone "0123456789"
-		address "123 My Address"
-		description "My Description"
-		total 1000.01
-	end
+  factory :order do
+    email "test@mail.com"
+    phone "0123456789"
+    address "123 My Address"
+    description "My Description"
+    total 1000.01
+  end
 end

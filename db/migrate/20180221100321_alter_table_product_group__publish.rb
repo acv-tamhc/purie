9c989dd3998ec59a0a702b4b8ac67ca99e5966ac
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-04 00:27:26 +0700

    fix text indent - dbmigrate

--- db/migrate/20180221100321_alter_table_product_group__publish.rb
+++ db/migrate/20180221100321_alter_table_product_group__publish.rb
@@ -1,8 +1,8 @@
 class AlterTableProductGroupPublish < ActiveRecord::Migration[5.1]
   def up
-  	add_column :product_groups, :published, :boolean, :default => true
+    add_column :product_groups, :published, :boolean, :default => true
   end
   def down
-  	remove_column :product_groups, :published, :boolean 
+    remove_column :product_groups, :published, :boolean 
   end
 end
--- db/migrate/20180221112929_add_reference_product_group__in__product_group.rb
+++ db/migrate/20180221112929_add_reference_product_group__in__product_group.rb
@@ -1,8 +1,8 @@
 class AddReferenceProductGroupInProductGroup < ActiveRecord::Migration[5.1]
   def up
-  	add_reference :product_groups, :product_group, foreign_key: true
+    add_reference :product_groups, :product_group, foreign_key: true
   end
   def down
-  	remove_column :product_groups, :product_group_id
+    remove_column :product_groups, :product_group_id
   end
 end

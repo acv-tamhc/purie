e17b0c7f3c04828597701264060fdf262c8d8c5d
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-05 00:30:21 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'home#index'
-  match '/cart', to: 'cart#cart', :as => 'cart', via: :all
-  match '/cart/address', to: 'cart#address', :as => 'cart_address', via: :all
-  match '/cart/checkout', to: 'cart#checkout', :as => 'cart_checkout', via: :all
-  get 'products/:id', to: 'products#show', :as => 'product_detail'
-  get 'all-products', to: 'categories#show', :as => 'category_all'
-  get 'category/:id', to: 'categories#show', :as => 'category_detail'
+  # match '/cart', to: 'cart#cart', :as => 'cart', via: :all
+  # match '/cart/address', to: 'cart#address', :as => 'cart_address', via: :all
+  # match '/cart/checkout', to: 'cart#checkout', :as => 'cart_checkout', via: :all
+  resources :carts, path_names: { index: 'cart', new: 'address',  cart: 'checkout'}
+  resources :categories
+  resources :products
 end

e7527c069f312b1503ccc510d4aa4d68580d5da4
Huynh Tam <tamhc@atmarkcafe.org>
2018-05-05 00:30:46 +0700

    update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'home#index'
-  # match '/cart', to: 'cart#cart', :as => 'cart', via: :all
-  # match '/cart/address', to: 'cart#address', :as => 'cart_address', via: :all
-  # match '/cart/checkout', to: 'cart#checkout', :as => 'cart_checkout', via: :all
   resources :carts, path_names: { index: 'cart', new: 'address',  cart: 'checkout'}
   resources :categories
   resources :products
